c774a3c1a6ccae1045562b1e541cb4599eff033c
CrazM13 <sepulvedajoey13@yahoo.com>
2023-01-22 13:30:04 -0500

    Initial Project Setup

---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Services/ServiceLocator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour {
+	// Readonly services
+	public static SceneTransition @SceneManager { get; set; }
+
+	// Singleton
+	private static ServiceLocator instance;
+
+	private void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this);
+			return;
+		}
+		instance = this;
+		LocateServices();
+	}
+
+	private void LocateServices() {
+		@SceneManager = FindObjectOfType<SceneTransition>();
+	}
+
+	private void OnDestroy() {
+		if (instance == this) {
+			@SceneManager = null;
+		}
+	}
+}
