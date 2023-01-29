a2ec857bfd76fa079e7533cdd6bd6502ca6c193c
Rui Santos <cardkingrui@gmail.com>
2023-01-29 17:55:21 -0500

    Made Several Things
    Created Winscene for testing, Created two scripts to use as the Plant object and Plant Manager. Ctreated Plant Manager and Plant Prefabs. Updated Tag Manager to include Manager and Plant Tags for List purposes on Start.

---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Plant_Scripts/Nibbleable_Object.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nibbleable_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] bool canNibble = true;
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] int currentHealth;
+    [SerializeField] GameObject manager;
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("Manager");
+        currentHealth = maxHealth;
+    }
+    private void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            canNibble = false;
+            manager.GetComponent<Plant_Manager>().removePlant(this.gameObject);
+            Object.Destroy(this.gameObject, 1);
+        }
+    }
+
+    public void gettingNibbled(int attack)
+    {
+        if (canNibble == true)
+        {
+            currentHealth = currentHealth - attack;
+            if (currentHealth <= 0)
+            {
+                canNibble = false;
+                this.gameObject.SetActive(false);
+                Object.Destroy(this.gameObject, 1);
+            }
+        }
+    }
+}
---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Plant_Scripts/Plant_Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Plant_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] List<GameObject> plants;
+    [SerializeField] bool survivingPlants;
+    void Start()
+    {
+        plants = new List<GameObject>(UnityEngine.GameObject.FindGameObjectsWithTag("Plant"));
+        if (plants.Count > 0)
+        {
+            survivingPlants = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (plants.Count <= 0)
+        {
+            survivingPlants = false;
+        }
+        checkIfWon();
+    }
+
+    void checkIfWon()
+    {
+        if (survivingPlants == false)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void removePlant(GameObject gameObject)
+    {
+        plants.Remove(gameObject);
+    }
+}
