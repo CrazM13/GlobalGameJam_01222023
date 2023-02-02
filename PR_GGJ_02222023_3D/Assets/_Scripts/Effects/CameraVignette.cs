3dbc6f592f9a811951be8bd90c8a7a79cab8c542
CrazM13 <sepulvedajoey13@yahoo.com>
2023-02-01 22:32:37 -0500

    Added screen effect when near enemy

---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Effects/CameraVignette.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class CameraVignette : ScriptableRendererFeature {
+
+	private class CameraVignettePass : ScriptableRenderPass {
+		private Material material;
+		private Mesh mesh;
+
+		public CameraVignettePass(Material material, Mesh mesh) {
+			this.material = material;
+			this.mesh = mesh;
+		}
+
+		public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+			CommandBuffer cmd = CommandBufferPool.Get(name: "CameraVignettePass");
+
+			cmd.SetViewProjectionMatrices(Matrix4x4.identity, Matrix4x4.identity);
+
+			cmd.DrawMesh(this.mesh, Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(2, 2, 1)), this.material, 0, 0);
+			context.ExecuteCommandBuffer(cmd);
+
+			CommandBufferPool.Release(cmd);
+		}
+	}
+
+	public bool IsEffectActive {
+		set {
+			material.SetInt("_Active", value ? 1 : 0);
+		}
+	}
+
+	public float Strength {
+		set {
+			material.SetFloat("_Strength", value);
+		}
+	}
+
+	public float Scale {
+		set {
+			material.SetFloat("_Scale", value);
+		}
+	}
+
+	[SerializeField] private Material material;
+	[SerializeField] private Mesh mesh;
+
+	private CameraVignettePass cameraVignettePass;
+
+	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+		if (material != null && mesh != null) {
+			renderer.EnqueuePass(cameraVignettePass);
+		}
+	}
+
+	public override void Create() {
+		cameraVignettePass = new CameraVignettePass(material, mesh);
+	}
+}
---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Effects/CameraVignetteController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVignetteController : MonoBehaviour {
+
+	[SerializeField] private CameraVignette vignette;
+	[SerializeField] private float dangerDistance;
+
+	private List<Transform> dangers = new List<Transform>();
+
+	private bool isEffectActive = false;
+
+	// Start is called before the first frame update
+	void Start() {
+		vignette.IsEffectActive = false;
+
+		foreach (AIEnemy enemy in FindObjectsOfType<AIEnemy>()) {
+			dangers.Add(enemy.transform);
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float distanceToDanger = GetNearestTargetDanger(transform.position);
+
+		if (distanceToDanger < dangerDistance) {
+			if (!isEffectActive) vignette.IsEffectActive = isEffectActive = true;
+			vignette.Strength = 2 - (distanceToDanger / dangerDistance);
+			vignette.Scale = 1 - (distanceToDanger / dangerDistance);
+
+
+		} else if (isEffectActive) {
+			vignette.IsEffectActive = isEffectActive = false;
+		}
+	}
+
+	private float GetNearestTargetDanger(Vector3 startPosition) {
+
+		float closestDistance = float.MaxValue;
+
+		foreach (Transform danger in dangers) {
+			if (!danger) continue;
+
+			float newDistance = Vector3.Distance(startPosition, danger.position);
+			if (newDistance < closestDistance) {
+				closestDistance = newDistance;
+			}
+		}
+
+		return closestDistance;
+	}
+}
