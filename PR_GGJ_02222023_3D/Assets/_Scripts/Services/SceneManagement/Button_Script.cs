28165048a3258d199bf07d22b48d33d2b966e23f
Rui Santos <cardkingrui@gmail.com>
2023-02-02 19:46:40 -0500

    Added Sprites and Button Script
    Added in Models and Sprites for some Vegetables. Added in Button Script for changing scenes.

---
+++ PR_GGJ_02222023_3D/Assets/_Scripts/Services/SceneManagement/Button_Script.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Script : MonoBehaviour
+{
+    public void mainMenu()
+    {
+        ServiceLocator.SceneManager.LoadSceneByName("MainMenu");
+    }
+
+    public void winScene()
+    {
+        ServiceLocator.SceneManager.LoadSceneByName("WinScene");
+    }
+
+    public void loseScene()
+    {
+        ServiceLocator.SceneManager.LoadSceneByName("LoseScene");
+    }
+
+    public void options()
+    {
+        ServiceLocator.SceneManager.LoadSceneByName("Options");
+    }
+
+    public void howToPlay()
+    {
+        ServiceLocator.SceneManager.LoadSceneByName("HowToPlay");
+    }
+}
