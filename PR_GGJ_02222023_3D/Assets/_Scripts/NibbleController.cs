46998843552099a67107e1a2de0019184a25a6cb
Brown2Snoopy <clank4491@gmail.com>
2023-02-02 21:02:01 -0500

    added range to nibble

--- PR_GGJ_02222023_3D/Assets/_Scripts/NibbleController.cs
+++ PR_GGJ_02222023_3D/Assets/_Scripts/NibbleController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class NibbleController : MonoBehaviour {
 
 	public int dmg;
+
+	public float range = 10f;
+
 	// Update is called once per frame
 	void Update() {
 		if (Input.GetButtonDown("Fire1")) {
-			if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, float.MaxValue, -1, QueryTriggerInteraction.Collide)) {
+			if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, range, -1, QueryTriggerInteraction.Collide)) {
 				Nibbleable_Object nibbleable = hit.collider.GetComponent<Nibbleable_Object>();
 
 				if (nibbleable) {
